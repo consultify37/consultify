3a04670be28823bbe236cb309e61e28f10bb5d31
Andrei Radu <andrei1.radu93@gmail.com>
2024-11-13 14:12:22 +0200

    .

--- next.config.js
+++ next.config.js
@@ -33,6 +33,13 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+      config.module.rules.push({
+        test: /\.node/,
+        use: "raw-loader",
+      });
+  return config;
+    },
 };
 
 module.exports = nextConfig;
