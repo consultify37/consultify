15871a14e08f040f62db1849ef37c142c2a4c1cd
Andrei Radu <andrei1.radu93@gmail.com>
2024-12-24 12:15:02 +0200

    .

--- pages/shop/success/index.tsx
+++ pages/shop/success/index.tsx
@@ -16,18 +16,14 @@ const Success = () => {
         <h1 className='lg:text-[32px] font-bold text-secondary'>Ți-am trimis un email atât cu produsele, cât și cu pașii de utilizare.</h1>
         <h1 className='lg:text-[32px] font-bold text-secondary'>De asemenea, produsele sunt disponibile și în { currentUser ? <Link href='/cont/comenzi' className='underline underline-offset-1'>contul tău</Link> : 'contul tău'}.</h1>
 
-        { !currentUser && 
-          <>
-            <Link 
-              href={'/login'}
-              className='px-16 py-3 lg:py-4 w-full max-w-[400px] bg-primary flex items-center justify-center rounded-full hover:scale-105 transition-all mt-8'
-            >
-              <p className='text-onPrimary font-semibold text-[14px] lg:text-16px'>Intră în cont</p>
-            </Link>
+        <Link 
+          href={currentUser ? '/cont' : '/login'}
+          className='px-16 py-3 lg:py-4 w-full max-w-[400px] bg-primary flex items-center justify-center rounded-full hover:scale-105 transition-all mt-8'
+        >
+          <p className='text-onPrimary font-semibold text-[14px] lg:text-16px'>Intră în cont</p>
+        </Link>
 
-            <p className='text-[#959595] text-[14px] text-center max-w-[420px] mt-8'>Dacă până acum nu ai avut cont pe platforma Inspirely, ți-am creat unul nou, iar datele de logare le găsești pe mail.</p>
-          </>
-        }
+        <p className='text-[#959595] text-[14px] text-center max-w-[420px] mt-8'>Dacă până acum nu ai avut cont pe platforma Inspirely, ți-am creat unul nou, iar datele de logare le găsești pe mail.</p>
 
         <Image
           src='/images/success/Polygon 3 (5).svg'
