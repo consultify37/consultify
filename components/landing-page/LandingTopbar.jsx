cbf64d5266ada99dfd81498853c0732454018af0
Andrei Radu <andrei1.radu93@gmail.com>
2025-02-11 15:54:03 +0200

    .

--- components/landing-page/LandingTopbar.jsx
+++ components/landing-page/LandingTopbar.jsx
@@ -3,22 +3,22 @@ import React from 'react'
 const LandingTopbar = () => {
   return (
     <div className='fixed w-full mx-auto z-20 px-4 py-2 bg-secondary text-onSecondary font-semibold overflow-hidden whitespace-nowrap'>
-      <p className='logos-slide'>🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% ! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
-      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% ! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
-      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% ! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
-      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% ! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
-      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% ! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
-      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% ! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
-      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% ! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
-      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% ! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
-      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% ! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
-      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% ! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
-      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% ! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
-      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% ! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
-      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% ! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
-      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% ! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
-      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% ! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
-      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% ! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
+      <p className='logos-slide'>🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% în luna februarie! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
+      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% în luna februarie! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
+      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% în luna februarie! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
+      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% în luna februarie! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
+      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% în luna februarie! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
+      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% în luna februarie! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
+      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% în luna februarie! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
+      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% în luna februarie! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
+      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% în luna februarie! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
+      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% în luna februarie! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
+      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% în luna februarie! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
+      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% în luna februarie! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
+      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% în luna februarie! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
+      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% în luna februarie! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
+      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% în luna februarie! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
+      🚚 Transport gratuit la orice comandă! | 🎉 Reducere 50% în luna februarie! | 🙏🏻 Vă mulțumim pentru toate comenzile! |
       </p>
     </div>
   )
