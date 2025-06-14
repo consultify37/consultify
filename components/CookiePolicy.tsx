2548b595c9d0e8120a1eee181e858f6c0305ab6e
Andrei Radu <andrei1.radu93@gmail.com>
2025-06-14 11:28:15 +0300

    .

--- components/CookiePolicy.tsx
+++ components/CookiePolicy.tsx
@@ -39,7 +39,7 @@ const CookiePolicy = () => {
         </>
       }
       {
-        (!consentRequested && loaded) ?
+        (consentRequested == null && loaded) ?
         <div className="bg-white fixed bottom-6 md:bottom-8 right-0 rounded-xl shadow-2xl p-8 flex flex-col items-center z-[9999] mx-4 w-[calc(100%-32px)] max-w-[440px]">
           <div className="flex flex-row justify-between items-center w-full">
             <div className="flex flex-row items-center">
